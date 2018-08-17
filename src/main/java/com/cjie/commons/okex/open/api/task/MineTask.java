ddd3e439f36ebbeef21376550a63a3a36802f66b
luhongli <luhongli24@163.com>
2018-08-17 12:54:38 +0800

    update

--- src/main/java/com/cjie/commons/okex/open/api/task/MineTask.java
+++ src/main/java/com/cjie/commons/okex/open/api/task/MineTask.java
@@ -5,9 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Slf4j
 public class MineTask {
 
