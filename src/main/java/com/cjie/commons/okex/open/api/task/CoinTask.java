55839963777f355fbdb7e40edb309a1dcc7d4f15
luhongli <luhongli24@163.com>
2018-08-20 17:14:52 +0800

    uopdate stop coin trade

--- src/main/java/com/cjie/commons/okex/open/api/task/CoinTask.java
+++ src/main/java/com/cjie/commons/okex/open/api/task/CoinTask.java
@@ -4,7 +4,6 @@ package com.cjie.commons.okex.open.api.task;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,7 +13,7 @@ public class CoinTask {
     @Autowired
     private CoinService coinService;
 
-    @Scheduled(cron = "*/3 * * * * ?")
+    //@Scheduled(cron = "*/3 * * * * ?")
     public void mineCurrency1() throws JobExecutionException {
         CoinTask.log.info("start mining");
         try {
